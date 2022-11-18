6354a708ce1341e958a2360cbce5c8767899a844
LOGEDI <nelsonlogedi@gmail.com>
2022-11-18 21:01:21 +0000

    my first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id= Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    planet_id = Column(Integer,ForeignKey('planet.id'))
+    character_id = Column(Integer,ForeignKey('people.id'))
+    #person = relationship(Person)
 
-class Address(Base):
-    __tablename__ = 'address'
+    
+
+class Favoriteplanet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer,ForeignKey('user.id'))
+   # person = relationship(Person)
+
+      
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer,primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+   # person = relationship(Person)
+
+    
+
+class FavoriteCharacter(Base):
+    __tablename__ = 'favorite_Character'
+    id = Column(Integer, primary_key=True)
+    user_id =Column(Integer,ForeignKey('user.id'))
+    character_id = Column(Integer,ForeignKey('character.id'))
+   # person = relationship(Person)
+         
+   
+
+#class Favorite(Base):
+   # __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    #id = Column(Integer, primary_key=True)
+   # user_id = Column(Integer,ForeignKey('user.id'))
+    #planet_id = Column(Integer, ForeignKey('planet.id'))
+   # person_id = Column(Integer, ForeignKey('person.id'))
+    #fav_id = Column (Integer,ForeignKey('fav.id'))
+    #person = relationship(Person)
 
     def to_dict(self):
         return {}
